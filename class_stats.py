325b99027ee7fce864005de7c8c5c9e417d1aee3
marlena-rusin <110559599+marlena-rusin@users.noreply.github.com>
2022-11-29 22:35:10 +0100

    Delete class_stats.py

--- class_stats.py
+++
@@ -1,7 +0,0 @@
-class Stats:
-    def __init__(self, bulls, cows):
-        self.bulls = bulls
-        self.cows = cows
-        
-    def show_score(self):
-        return print(f"{self.bulls} Bulls, {self.cows} Cows")
